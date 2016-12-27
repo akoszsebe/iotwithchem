f3cc1c7be516ba01fab923dbd5a34b351ccb7320
Zsebe Akos <zsebea@yahoo.com>
2016-12-27 15:34:29 +0200

    added calibration page

--- frontend/src/app.route.js
+++ frontend/src/app.route.js
@@ -23,6 +23,15 @@
 			authenticate: true
 		}
 
+		let calibrationState = {
+			name: 'calibration',
+			url: '/calibration',
+			templateUrl: 'frontend/src/calibration/calibration.html',
+			controller: 'calibrationController',
+			controllerAs: 'calibration',
+			authenticate: true
+		}
+
 		let teamState = {
 			name: 'team',
 			url: '/team',
@@ -48,6 +57,7 @@
 
 		$stateProvider.state(helloState)
 		$stateProvider.state(researchState)
+		$stateProvider.state(calibrationState)
 		$stateProvider.state(teamState)
 		$stateProvider.state(loginState)
 
---
+++ frontend/src/calibration/calibration.controller.js
@@ -0,0 +1,54 @@
+(() => {
+	angular.module('kemia-app')
+	.controller('calibrationController', calibrationController)
+
+	calibrationController.$inject = ['$scope', 'calibrationFactory','$interval','$window']
+
+	function calibrationController($scope, calibrationFactory,$interval,$window) {
+		let temp=this
+		temp.ph=0.0
+		getPh().then(() => {
+		})
+
+		function getPh() {
+			return calibrationFactory.getPh()
+			.then((ph) => {
+				temp.ph = ph
+				$scope.phValue=temp.ph.phvalue
+				$scope.phDate=temp.ph.phdate
+				return temp.ph
+			})
+		}
+		$interval(getPh, 3000)
+
+		$scope.calibrationlow=function()
+        {
+			//$window.alert('mak')
+			calibrationlow()
+		}
+
+		function calibrationlow() {      
+			return calibrationFactory.calibratephsensorlow()
+		}
+
+		$scope.calibrationmid=function()
+        {
+			//$window.alert('mak')
+			calibrationmid()
+		}
+
+		function calibrationmid() {      
+			return calibrationFactory.calibratephsensormid()
+		}
+
+		$scope.calibrationhigh=function()
+        {
+			//$window.alert('mak')
+			calibrationhigh()
+		}
+
+		function calibrationhigh() {      
+			return calibrationFactory.calibratephsensorhigh()
+		}
+	}
+})()
