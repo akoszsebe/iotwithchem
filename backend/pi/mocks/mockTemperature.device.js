3b35297b89c04aecca5783b27861ec1bedee827c
Vincze Dávid <vincdave@yahoo.com>
2017-03-22 21:08:15 +0200

    Made the temperature and ph mocks configurable

--- backend/pi/mocks/mockTemperature.device.js
+++ backend/pi/mocks/mockTemperature.device.js
@@ -1,3 +1,21 @@
+/*
+INFO USING THE INTERVAL SIMULATION
+The return values are simulated, wiht the help of the config JSON.
+If you want a fix point returned every time, give 'fix' value to config type. In this case it will allways return the value stored in fixPoint.
+If you want to simulate a changing in the given interval, change the type to 'interval' and modify the interval value to your [minimum,maximum] and the intervalDiff to the change in difference.
+It will go from the middle of the 2 intervals, increasing to the max interval then decreasing to min interval, and repeating the same sequence.
+*/
+
+var config = {
+	type: 'interval', //fix or interval
+	fixPoint: 15, // ignore this if using interval
+	interval: [20,30],
+	intervalDiff: 0.1
+}
+
+var myCurrentValue = (config['interval'][0]+config['interval'][1])/2
+
+
 /**
  * Class to handle all the TemperatureDevice  sensors
  */
@@ -10,5 +28,15 @@ var TemperatureDevice = module.exports = function () {
  * @returns err - if error happened , value - the temperature value
  */
 TemperatureDevice.prototype.actualValue = function(_callback){
-	return _callback(null, 25);
+	if(config['type']=='fix'){
+		return _callback(null, config['fixPoint'])
+	}
+	else
+	{
+		myCurrentValue=(myCurrentValue + config['intervalDiff'])
+		if(myCurrentValue > config['interval'][1] || myCurrentValue < config['interval'][0]){
+			config['intervalDiff'] = -config['intervalDiff']
+		}
+		return _callback(null,myCurrentValue.toFixed(2))
+	}
 }
