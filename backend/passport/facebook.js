baf08e42117897ab82a1474a6ad848b30e65bcc7
Kelemen Istvan <kelemenisty@gmail.com>
2017-04-01 19:34:20 +0300

    optimized fb login method

--- backend/passport/facebook.js
+++ backend/passport/facebook.js
@@ -10,7 +10,7 @@ module.exports = (passport) => {
       clientID: '190103424780365',
       clientSecret: '6f2b8d730b04ae86285aac21bbfe1e41',
       callbackURL: 'https://iotwithchem2.herokuapp.com/login/facebook/return',
-      profileFields: ['id', 'name', 'link', 'about', 'email']
+      profileFields: ['id', 'name', 'link', 'about', 'email', 'photos']
     },
     // facebook will send back the tokens and profile
     (access_token, refresh_token, profile, done) => {
@@ -19,55 +19,42 @@ module.exports = (passport) => {
         fb.api('/687797544718797/members?access_token=' + access_token, (response) => {
           if (response.error) return done(response.error);
           if (response) {
-            let exists = false;
+            let isMember = false;
             response.data.forEach((user) => {
               if (user.id === profile.id) {
-                exists = true;
-                fb.api('/' + user.id + '/picture?redirect=0', function (response) {
-                  if (response.error) console.log(response.error);
-                  else {
-                    let picUrl = response.data.url;
-                    User.findOne({
-                      'fb.id': profile.id
-                    }, (err, user) => {
+                isMember = true;
+
+                User.findOne({
+                  'fb.id': profile.id
+                }, (err, user) => {
+                  if (err) {
+                    console.log(err);
+                    return done(err)
+                  } else if (user) {
+                    console.log('exists in the db');
+                    return done(null, user)
+                  } else {
+                    let newUser = new User();
+                    newUser.fb.id = profile.id;
+                    newUser.fb.name = profile.name.givenName + ' ' + profile.name.familyName;
+                    newUser.fb.email = profile.emails[0].value;
+                    newUser.fb.picUrl = profile.photos[0].value;
+                    newUser.save(function (err) {
                       if (err) {
-                        console.log(err);
-                        return done(err)
-                      } else if (user) {
-                        if (picUrl !== user.fb.picUrl) {
-                          user.fb.picUrl = picUrl;
-                          user.update(function (err) {
-                            if (err) throw err
-                          });
-                        }
-                        console.log('exists in the db');
-                        return done(null, user)
+                        throw err
                       } else {
-                        let newUser = new User();
-                        newUser.fb.id = profile.id;
-                        newUser.fb.access_token = access_token;
-                        newUser.fb.name = profile.name.givenName + ' ' + profile.name.familyName;
-                        newUser.fb.email = profile.emails[0].value;
-                        newUser.fb.picUrl = picUrl;
-                        console.log(picUrl);
-                        newUser.save(function (err) {
-                          if (err) {
-                            throw err
-                          } else {
-                            return done(null, newUser)
-                          }
-                        })
+                        return done(null, newUser)
                       }
-                    })
+                    });
                   }
-                })
+                });
               }
             });
-            if (!exists) {
+            if (!isMember) {
               return done(null, false)
             }
           }
-        })
-      })
-    }))
+        });
+      });
+    }));
 };
