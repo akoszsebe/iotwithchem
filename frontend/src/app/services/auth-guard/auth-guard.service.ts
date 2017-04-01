7b4003cd81821223e8cc1e702a0f7d1072791f67
Kelemen Istvan <kelemenisty@gmail.com>
2017-04-01 21:18:35 +0300

    added route guarding

--- frontend/src/app/services/auth-guard/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
-import {Observable} from 'rxjs/observable';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
