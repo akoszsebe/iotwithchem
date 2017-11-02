00bf550c764f7f22e317f4bc7f84b0be4793afb6
Zsebe Akos <zsebea@yahoo.com>
2017-11-02 11:49:22 +0200

    commented authentification

--- frontend/src/app/services/auth-guard/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard/auth-guard.service.ts
@@ -13,22 +13,23 @@ export class AuthGuardService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
 
     const url: string = state.url;
-    return this.authService.checkAuthentication().map(user => {
-      if (user !== null) {
-        localStorage.setItem('name', user.name);
-        this.authService.setUser(user);
-      }
-      if (url === '/login') {
-        return !user;
-      } else {
-        if (user) {
-          return true;
-        }
-        this.authService.redirectUrl = url;
-        this.router.navigate(['/login']);
-        // need to be false // // is set to true for skip authentification//
-        return true;
-      }
-    });
+    return true;
+    // return this.authService.checkAuthentication().map(user => {
+    //   if (user !== null) {
+    //     localStorage.setItem('name', user.name);
+    //     this.authService.setUser(user);
+    //   }
+    //   if (url === '/login') {
+    //     return !user;
+    //   } else {
+    //     if (user) {
+    //       return true;
+    //     }
+    //     this.authService.redirectUrl = url;
+    //     this.router.navigate(['/login']);
+    //     // need to be false // // is set to true for skip authentification//
+    //     return true;
+    //   }
+    // });
   }
 }
