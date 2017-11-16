91572652a0f7e5b3cda451fb47824ee221b41de2
Zsebe Akos <zsebea@yahoo.com>
2017-11-16 18:42:20 +0200

    slide nav useradded1

--- frontend/src/app/components/experiment/experiment.component.ts
+++ frontend/src/app/components/experiment/experiment.component.ts
@@ -145,12 +145,11 @@ export class ExperimentComponent implements OnInit, OnDestroy {
     });
     this.connection3 = this.deviceService.getDeviceStatusChanges().subscribe(response => {
       this.msg = [];
-      this.toggleChecked = response;
+      this.toggleChecked = true;//response
       this.toggleSync();
-      this.deviceConnected = response;
+      this.deviceConnected =true;// response;
       response ? this.msg = [{severity: 'success', summary: 'Device', detail: 'Pi connected'}]
-        ://this.msg = [{severity: 'error', summary: 'Device', detail: 'Pi disconnected'}];
-        this.msg = [{severity: 'success', summary: 'Device', detail: 'Pi connected'}];
+        :this.msg = [{severity: 'error', summary: 'Device', detail: 'Pi disconnected'}];
     });
 
     this.jobService.getJob()
